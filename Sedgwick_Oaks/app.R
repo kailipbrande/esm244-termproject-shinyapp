93bf79dbc956cab1628fb0b2ddd001196c5d912a
taylorlockmann <tlockmann@gmail.com>
2021-02-06 22:42:13 -0800

    Added tabular structure & placeholder panels.

--- Sedgwick_Oaks/app.R
+++ Sedgwick_Oaks/app.R
@@ -1,49 +1,44 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
+# Define UI for our application
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    navbarPage("Sedgwick Oaks",
+               tabPanel("Overview"),
+               tabPanel("Widget 1",
+                        sidebarLayout(
+                            sidebarPanel("Species"),
+                            mainPanel("2020 Distribution")
+                        )
+                        ),
+               tabPanel("Widget 2",
+                        sidebarLayout(
+                            sidebarPanel("Select Year"),
+                            mainPanel("Species Distribution")
+                        )
+                        ),
+               tabPanel("Widget 3",
+                        sidebarLayout(
+                            sidebarPanel("Select Year"),
+                            mainPanel("Number of live individuals")
+                        )
+                        ),
+               tabPanel("Widget 4",
+                        sidebarLayout(
+                            sidebarPanel("Select time period"),
+                            mainPanel("Number of live individuals")
+                        )
+                        )
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# Define server
+server <- function(input, output) {}
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
