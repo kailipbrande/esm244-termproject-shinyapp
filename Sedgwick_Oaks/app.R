96a9edd24bc3db0e0dc5fb3fb1c0017f77da3f06
elmeraa <elmera@ucsb.edu>
2021-03-08 20:05:05 -0800

    final additions to app

--- Sedgwick_Oaks/app.R
+++ Sedgwick_Oaks/app.R
@@ -37,30 +37,31 @@ ui <- fluidPage(theme = sedgwick_theme,
                           sidebarPanel(
                             radioButtons(inputId = "select_species", label = "Select Species:", choices = list( "QULO", "QUDO", "QUAG"), selected = "QULO")),
                           mainPanel("2020 Distribution",
-                                    plotOutput(outputId = "species_plot"))
+                                    plotOutput(outputId = "widget1plot", height = "100%", width = "100%"))
                         )
                ),
                tabPanel("Widget 2",
                         sidebarLayout(
-                          sidebarPanel("Select Year",
+                          sidebarPanel(
                                        checkboxGroupInput(inputId = "pick_year",
-                                                          label = "Select study year:",
+                                                          label = "Select Study Year:",
                                                           choices = unique(tree_melt$year),
+                                                          selected = "1938"
                                        )),
-                          mainPanel("Species Distribution",
-                                    plotOutput("widget2plot", height = "200%", width = "200%")),
+                          mainPanel("Species Distribution by Selected Year",
+                                    plotOutput("widget2plot", height = "100%", width = "100%")),
                         )
                ),
                tabPanel("Widget 3",
                         sidebarLayout(
                           sidebarPanel(
                             selectInput(inputId = "select_year", label = "Select Year:", choices = list("1938" = 1938, "1943" = 1943, "1954" = 1954, "1967" = 1967, "1980" = 1980, "1994" = 1994, "2004" = 2004, "2012" = 2012, "2014" = 2014, "2016" = 2016, "2018" = 2018, "2020" = 2020), selected = 1)),
-                          mainPanel("Number of live individuals",plotOutput(outputId = "widget3_plot")))
+                          mainPanel("Number of Live Individuals",plotOutput(outputId = "widget3_plot")))
                ),
 
                 tabPanel("Widget 4",
                          sidebarLayout(
-                             sidebarPanel("Select Time Period",
+                             sidebarPanel("Select Time Period:",
                                                    sliderInput("slider2", label = h3("Slider Range"),
                                                                min = 1938, max = 2020,
                                                                value = c(1954, 2014),
@@ -83,11 +84,13 @@ server <- function(input, output) {
 
   })
 
-  output$species_plot <- renderPlot({
-    ggplot(data = widget1reactive())+
-      geom_sf()
-
-  })
+  output$widget1plot <- renderPlot({
+    ggplot()+
+      geom_sf(data = widget1reactive(), size = 1) +
+      theme_minimal() +
+      labs(x = "Longitude", y = "Latitude") +
+      theme(axis.text.x = element_text(angle = 90))
+  }, height = 500, width = 900)
 
  widget2reactive <- reactive({
     tree_melt %>%
@@ -97,13 +100,14 @@ server <- function(input, output) {
 
   output$widget2plot <- renderPlot({
     ggplot() +
-      geom_sf(data = widget2reactive(), aes(color = species), size = 3) +
+      geom_sf(data = widget2reactive(), aes(color = species), size = 1) +
       theme_minimal() +
       labs(x = "Longitude", y = "Latitude") +
       scale_colour_discrete(name = "Species:") +
       scale_color_brewer(palette = "Dark2" ) +
-      theme(legend.position="top")
-  }, height = 800, width = 1300)
+      theme(legend.position="top") +
+      theme(axis.text.x = element_text(angle = 90))
+  }, height = 500, width = 900)
 
 
     widget3_reactive <- reactive({
@@ -114,7 +118,10 @@ server <- function(input, output) {
 
     output$widget3_plot <- renderPlot({
       ggplot(data = widget3_reactive(), aes(x = year, y = n, fill = species)) +
-        geom_bar(stat="identity", width=.5, position = "dodge")
+        geom_bar(stat="identity", width=.5, position = "dodge") +
+        theme_minimal() +
+        labs( x = "Year", y = "Count") +
+        scale_fill_brewer(palette = "Dark2" )
     })
 
 
@@ -130,7 +137,10 @@ server <- function(input, output) {
       output$widget4plot <- renderPlot({
         ggplot(data = widget4_reactive(), aes(x = year, y = count, color = species, group = species)) +
           geom_point() +
-          geom_line()
+          geom_line() +
+          theme_minimal() +
+          labs( x = "Year", y = "Count") +
+          scale_color_brewer(palette = "Dark2" )
       })
 
 
